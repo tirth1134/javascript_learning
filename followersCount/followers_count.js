3475910dd4b3456bf35996e4cc5955486501d9d6
tirth1134 <tirthvora1134@gmail.com>
2025-02-13 23:04:10 -0500

    message

---
+++ followersCount/followers_count.js
@@ -0,0 +1,22 @@
+let count=0;
+function increaseCount()
+{
+    count++;
+    displayCount();
+    checkCountValue();
+}
+function displayCount()
+{
+    document.getElementById('countDisplay').innerHTML=count;
+}
+function checkCountValue()
+{
+    if(count==10)
+    {
+        alert("You Gained 10 followers in Instagram. Congratulations..!!");
+    }
+    else if(count==20)
+    {
+        alert("You Gained 20 follwers in Instagram. Keep It Up👍");
+    }
+}
